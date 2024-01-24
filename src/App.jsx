65555720085294c73f714f074ae7aad6ad659f64
israelmitolu <israelmitolu@gmail.com>
2024-01-24 09:23:16 +0100

    Project setup ✨

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Companies from "./sections/Companies";
+import Hero from "./sections/Hero";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Companies />
+    </>
+  );
+}
+
+export default App;
